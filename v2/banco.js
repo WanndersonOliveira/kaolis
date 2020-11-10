8ff0d2c3fb60306e4bcf677e07949b79340ad755
Wannderson Nogueira <wannders.35@hotmail.com>
2020-11-10 13:58:23 -0300

    Adição de banco de dados externo

---
+++ v2/banco.js
@@ -0,0 +1,31 @@
+var produtos = [{
+   id:"produto",
+   titulo:"Copo Tupperware para viagem",
+   preco:250,
+   img:"../img/copo.jpg"
+},
+{id:"produto2",
+   titulo:"Pote Tupperware p/café",
+   preco:250,
+   img:"../img/pote.jpg"
+},
+{id:"produto3",
+   titulo:"Jarra Tupperware",
+   preco:250,
+   img:"../img/garrafa.jpg"
+},
+{id:"produto4",
+   titulo:"Jarra Tupperware",
+   preco:250,
+   img:"../img/jarra.jpg"
+},
+{id:"produto5",
+   titulo:"Descascador Tupperware",
+   preco:250,
+   img:"../img/desc.jpg"
+},
+{id:"produto6",
+   titulo:"Pote Tupperware p/feijão",
+   preco:250,
+   img:"../img/pote2.jpg"
+}];
