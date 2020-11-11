9e0a608792aa02258e07c1be64b8ffc88536d18c
Wannderson Nogueira <wannders.35@hotmail.com>
2020-11-11 10:05:42 -0300

    Adição de código relacionado à paginação

--- v2/banco.js
+++ v2/banco.js
@@ -2,30 +2,54 @@ var produtos = [{
    id:"produto",
    titulo:"Copo Tupperware para viagem",
    preco:250,
-   img:"../img/copo.jpg"
+   img:"../img/copo.jpg",
+   page: 1
 },
 {id:"produto2",
    titulo:"Pote Tupperware p/café",
    preco:250,
-   img:"../img/pote.jpg"
+   img:"../img/pote.jpg",
+   page: 1
 },
 {id:"produto3",
    titulo:"Jarra Tupperware",
    preco:250,
-   img:"../img/garrafa.jpg"
+   img:"",
+   page: 1
 },
 {id:"produto4",
    titulo:"Jarra Tupperware",
    preco:250,
-   img:"../img/jarra.jpg"
+   img:"../img/jarra.jpg",
+   page: 1
 },
 {id:"produto5",
    titulo:"Descascador Tupperware",
    preco:250,
-   img:"../img/desc.jpg"
+   img:"../img/desc.jpg",
+   page: 1
 },
 {id:"produto6",
    titulo:"Pote Tupperware p/feijão",
    preco:250,
-   img:"../img/pote2.jpg"
+   img:"",
+   page: 1
+},
+{id:"produto",
+   titulo:"Caneca 300 ml",
+   preco:250,
+   img:"../img/img_indisp.jpg",
+   page: 2
+},
+{id:"produto",
+   titulo:"Caneca 500 ml",
+   preco:50,
+   img:"../img/img_indisp.jpg",
+   page: 2
+},
+{id:"produto",
+   titulo:"Caneca 800 ml",
+   preco:250,
+   img:"",
+   page: 2
 }];
