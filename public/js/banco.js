097e4c8ed70d42f5c36df15b46a5daf1022351d5
Wannderson Nogueira <wannders.35@hotmail.com>
2020-11-13 13:16:01 -0300

    Separação do código css e js da página html

--- public/js/banco.js
+++ public/js/banco.js
@@ -2,13 +2,13 @@ var produtos = [{
    id:"produto",
    titulo:"Copo Tupperware para viagem",
    preco:250,
-   img:"../img/copo.jpg",
+   img:"img/copo.jpg",
    page: 1
 },
 {id:"produto2",
    titulo:"Pote Tupperware p/café",
    preco:250,
-   img:"../img/pote.jpg",
+   img:"img/pote.jpg",
    page: 1
 },
 {id:"produto3",
@@ -20,13 +20,13 @@ var produtos = [{
 {id:"produto4",
    titulo:"Jarra Tupperware",
    preco:250,
-   img:"../img/jarra.jpg",
+   img:"img/jarra.jpg",
    page: 1
 },
 {id:"produto5",
    titulo:"Descascador Tupperware",
    preco:250,
-   img:"../img/desc.jpg",
+   img:"img/desc.jpg",
    page: 1
 },
 {id:"produto6",
@@ -38,13 +38,13 @@ var produtos = [{
 {id:"produto",
    titulo:"Caneca 300 ml",
    preco:250,
-   img:"../img/img_indisp.jpg",
+   img:"img/img_indisp.jpg",
    page: 2
 },
 {id:"produto",
    titulo:"Caneca 500 ml",
    preco:50,
-   img:"../img/img_indisp.jpg",
+   img:"img/img_indisp.jpg",
    page: 2
 },
 {id:"produto",
