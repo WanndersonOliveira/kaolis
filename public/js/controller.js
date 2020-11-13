7ae392c6a5cb27b76667ad449ce515ad82c2d537
Wannderson Nogueira <wannders.35@hotmail.com>
2020-11-13 13:21:04 -0300

    Separação do código js da página html e criação do arquivo controller.js

---
+++ public/js/controller.js
@@ -0,0 +1,90 @@
+var app = angular.module('app',[]);
+	  app.controller('ctrl', function($scope){
+	      $scope.produtos = [];
+
+
+	      produtos.forEach(function (item,index){
+		    
+		    if(item.preco == 0){
+			item = null;
+		    }
+			
+		    if(!(item == null)){
+			if(item.img == ""){
+				item.img = "img/img_indisp.jpg";
+		    	}
+
+		     
+		    	item.preco = "R$ "+item.preco;
+
+			if(item.page == page){
+			   $scope.produtos.push(item);
+			}
+		    }
+		});
+
+		$("#prev").hide();
+		
+		$scope.next = function(){
+			page++;
+
+			$scope.produtos = [];
+
+
+	      		produtos.forEach(function (item,index){
+		    
+		    		if(item.preco == 0){
+					item = null;
+		    		}
+			
+		    		if(!(item == null)){
+					if(item.img == ""){
+						item.img = "img/img_indisp.jpg";
+		    		}
+
+
+				if(item.page == page){
+			   		$scope.produtos.push(item);
+				}
+		    	}
+			});
+
+			$("#prev").show();
+
+			if(produtos[(produtos.length-1)].page == page){
+				$("#next").hide();
+			}
+		}
+
+		
+		$scope.prev = function(){
+			page--;
+
+			$scope.produtos = [];
+
+
+	      		produtos.forEach(function (item,index){
+		    
+		    		if(item.preco == 0){
+					item = null;
+		    		}
+			
+		    		if(!(item == null)){
+					if(item.img == ""){
+						item.img = "img/img_indisp.jpg";
+		    		}
+
+
+				if(item.page == page){
+			   		$scope.produtos.push(item);
+				}
+		    	}
+			});
+
+			$("#next").show();
+
+			if(page == 1){
+				$("#prev").hide();
+			}
+		}
+	  });
